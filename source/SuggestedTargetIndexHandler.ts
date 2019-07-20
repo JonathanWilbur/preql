f949e28ea69d373a666accabfed86ce6b41db5bf
Jonathan M. Wilbur <jonathan@wilbur.space>
2019-07-20 07:37:01 -0400

    Make Suggested...Handlers return a string | object

--- source/SuggestedTargetIndexHandler.ts
+++ source/SuggestedTargetIndexHandler.ts
@@ -1,7 +1,17 @@
 import APIObjectDatabase from './Interfaces/APIObjectDatabase';
 import Logger from './Interfaces/Logger';
 
-type SuggestedTargetIndexHandler = (etcd: APIObjectDatabase, logger: Logger) => Promise<string>;
+/**
+ * This is the official recommended function signature for transpiling an
+ * entire `APIObjectDatabase`. It is only here to foster uniformity across
+ * all PreQL-related libraries.
+ *
+ * This is expected to return either a `string` when transpiling for
+ * a relational database, and return an `object` when transpiling a
+ * JSON-like schema (because objects can be trivially converted to JSON via
+ * `JSON.stringify()`.)
+ */
+type SuggestedTargetIndexHandler = (etcd: APIObjectDatabase, logger: Logger) => Promise<string | object>;
 
 // eslint-disable-next-line no-undef
 export default SuggestedTargetIndexHandler;
--- source/SuggestedTargetObjectHandler.ts
+++ source/SuggestedTargetObjectHandler.ts
@@ -1,7 +1,17 @@
 import APIObject from './Interfaces/APIObject';
 import Logger from './Interfaces/Logger';
 
-type SuggestedTargetObjectHandler = (obj: APIObject, logger: Logger) => Promise<string>;
+/**
+ * This is the official recommended function signature for transpiling an
+ * `APIObject`. It is only here to foster uniformity across all PreQL-related
+ * libraries.
+ *
+ * This is expected to return either a `string` when transpiling for
+ * a relational database, and return an `object` when transpiling a
+ * JSON-like schema (because objects can be trivially converted to JSON via
+ * `JSON.stringify()`.)
+ */
+type SuggestedTargetObjectHandler = (obj: APIObject, logger: Logger) => Promise<string | object>;
 
 // eslint-disable-next-line no-undef
 export default SuggestedTargetObjectHandler;
